a82296bdb7b5e8b4d447533fd7afeb5422ef3460
Vansh Talwar <vipultalwin99@gmail.com>
2026-04-22 18:40:30 +0530

    profile pic fixed

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -76,7 +76,7 @@ export const useAuthStore = create((set, get) => ({
       toast.success("Profile updated successfully");
     } catch (error) {
       console.log("error in update profile:", error);
-      toast.error(error.response.data.message);
+      toast.error(error.response.data.message  || "Upload failed");
     } finally {
       set({ isUpdatingProfile: false });
     }
